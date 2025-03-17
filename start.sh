6405e477291a16cbddc11511c93d3334dbab58cf
dl55459 <dl55459fer@gmail.com>
2025-03-18 00:35:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -e
 
-# Install ChromeDriver
+# Download and install Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
-apt-get install -y wget unzip
+apt-get install -y google-chrome-stable
+
+# Download and install ChromeDriver
 wget -N https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
 mv -f chromedriver /usr/local/bin/chromedriver
 rm chromedriver_linux64.zip
-
-# Install Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get update
-apt-get install -y google-chrome-stable
 
 # Install Python dependencies
 pip install -r requirements.txt
