24bc153f0cf82f82f2f0f22876a662f966329bc2
dl55459 <dl55459fer@gmail.com>
2025-03-17 22:26:17 +0100

    Update scraperMAP.py

--- scraperMAP.py
+++ scraperMAP.py
@@ -8,8 +8,8 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")  
 
 # Ensure ChromeDriver is correctly detected
-chrome_options.binary_location = "/usr/bin/chromium-browser"
-chrome_driver_path = "/usr/bin/chromedriver"
+chrome_options.binary_location = "/usr/bin/google-chrome"
+chrome_driver_path = "/usr/local/bin/chromedriver"
 
 
 # Start ChromeDriver service
