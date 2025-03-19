2c905dc1a73479209342dab42ec788ca2d82e5f9
dl55459 <dl55459fer@gmail.com>
2025-03-19 22:10:42 +0100

    Update wait-for-selenium.sh

--- wait-for-selenium.sh
+++ wait-for-selenium.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 until curl -s http://localhost:4444/wd/hub/status | jq -e '.value.ready' > /dev/null; do
     echo "Waiting for Selenium server..."
-    sleep 1
+    sleep 2
 done
+echo "Selenium server is ready!"
