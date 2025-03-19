7f5c0f1c5cbc2db85250ab8e4854e3e14c0e2188
dl55459 <dl55459fer@gmail.com>
2025-03-19 22:45:40 +0100

    Delete wait-for-selenium.sh

--- wait-for-selenium.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-until curl -s http://localhost:4444/wd/hub/status | jq -e '.value.ready' > /dev/null; do
-    echo "Waiting for Selenium server..."
-    sleep 2
-done
-echo "Selenium server is ready!"
